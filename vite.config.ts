ecf0822ab6afb110011f048b38b72b776bdd9aad
Vedant <Vedantarole18@gmail.com>
2026-04-22 02:11:43 +0530

    [skip ci]

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/devops/" : "/",   // 🔥 FIX
   plugins: [react()],
-});
+}));
